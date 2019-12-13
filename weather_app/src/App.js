b3c7e8f84205a7842499c819073312b7619d62f0
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2019-12-13 13:34:51 +0000

    make api call from weather api
    make api from weather and set necessary states for now. In addition, create a CityCard component where the information from the api will be shown.

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 class App extends React.Component {
 constructor(props) {
   super(props);
   this.state = {
+    citiesInfo:[],
+    searchInput:''
 
   }
+}
+// api weather url: https://samples.openweathermap.org/data/2.5/weather?q=London&appid=8532330ab1334c1ed18e9fa9480aa16c
+// key : 8532330ab1334c1ed18e9fa9480aa16c
+
+getWeather() {
+  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.searchInput}&appid=8532330ab1334c1ed18e9fa9480aa16c`)
+  .then(res => {
+    const citiesInfo = res.data;
+    this.setState({citiesInfo})
+  })
+}
+onChangeHandler = (event) => {
+  event.preventDefault();
+  this.setState({searchInput: event.target.value})
+}
+onClickHandler = () => {
+this.getWeather();
+console.log(this.state.citiesInfo)
 }
 
 
@@ -15,14 +42,22 @@ constructor(props) {
 
 
 
-
-  render () {
-    
- return (
-    <div className="App">
-      
+render () {
+  return (
+   <Router>
+     <Switch>
+       <Route exact path='/'>
+           <div className="App">
+             <img src='https://images.unsplash.com/photo-1516912481808-3406841bd33c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1283&q=80' alt='image by Jonathan Bowers'/>
+      <div className="search_input">
+        <input type="text" name="city" placeholder="search for city" onChange={this.onChangeHandler}/>
+        <input type="submit" value ="Submit" onClick={this.onClickHandler} />
+<h1>{this.state.citiesInfo.base}</h1>
+      </div>
     </div>
-  
+    </Route>
+    </Switch>
+  </Router>
   )
   
 }

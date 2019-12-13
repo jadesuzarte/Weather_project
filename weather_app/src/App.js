16535b608ec686e3e752cf2d2adb20c900507d06
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2019-12-13 11:43:46 +0000

    create create-react-app and add extensions
    add react router and axios to app

---
+++ weather_app/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {
+
+  }
+}
+
+
+
+
+
+
+
+
+  render () {
+    
+ return (
+    <div className="App">
+      
+    </div>
+  
+  )
+  
+}
+  
+}
+
+export default App;

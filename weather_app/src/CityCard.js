945df36e9d3c7ddb68dad8c263e1d94fff709ec9
Ines Guerrero <inesgs@gmail.com>
2019-12-16 16:15:31 +0000

    assessment comments

--- weather_app/src/CityCard.js
+++ weather_app/src/CityCard.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
 export function CityCard(props) {
-    let info = props.citiesInfo
-   let cityImage= props.image
+  let info = props.citiesInfo;
+  let cityImage = props.image;
 
-console.log(cityImage);
+  console.log(cityImage);
 
-    return (
-        <div className="weather_info">
-        <p>City: {info.name}</p>
-        <p>Base :{info.base}</p>
-        <p> Timezone:{info.timezone}</p>
-         <p> Visibility:{info.visibility}</p>
-         <img src={cityImage}/>
-        <div className="image_city">
-        </div>
-</div>
-    )
+  return (
+    <div className="weather_info">
+      <p>City: {info.name}</p>
+      <p>Base :{info.base}</p>
+      <p> Timezone:{info.timezone}</p>
+      <p> Visibility:{info.visibility}</p>
+      {/* This is not the image source this is the url that you fetch from. It works but the right image is not coming from state.  */}
+      <img src={cityImage} />
+      <div className="image_city"></div>
+    </div>
+  );
 }
